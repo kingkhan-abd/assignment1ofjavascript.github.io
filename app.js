4dbdfa7b5a18e08ef2ccba46efc5228168c7cb0e
kingkhan-abd <eijazking6791@gmail.com>
2023-02-07 17:09:57 +0500

    assignment1ofjavascrpt

---
+++ app.js
@@ -0,0 +1,17 @@
+alert("Welcome to our website thanks for your visit");
+alert("Please enter a valid message ");
+var fullname = "Welcome to JS Land...  \n  Happy Coding";
+alert(fullname);
+alert("Welcome to JS Land...\n ");
+alert("Happy Coding!\n Prevent this from creating additional dialogs. ");
+alert("Hello...I can run JS through my web browser's console");
+// swal("Hello world!");
+// swal("Welcome to our website thanks for your visit");
+// swal("Please enter a valid message ");
+// swal("Welcome to JS Land...  \n  Happy Coding");
+// swal("Welcome to JS Land...\n ");
+// swal("Happy Coding!\n Prevent this from creating additional dialogs. ");
+// swal("Hello...I can run JS through my web browser's console");
+
+
+
